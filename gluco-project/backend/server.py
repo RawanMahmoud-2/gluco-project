89f1d3a499502efd7197fa743ce71b762eb2b0de
RawanMahmoud-2 <rawan.mahmoud1231233@gmail.com>
2026-05-25 21:38:01 +0300

    Update server.py

--- gluco-project/backend/server.py
+++ gluco-project/backend/server.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-latest_ppg = None
+latest_ppg = []
 
+# Allow frontend requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "Backend running"}
 
 @app.post("/upload")
 async def upload(request: Request):
@@ -11,12 +24,12 @@ async def upload(request: Request):
     global latest_ppg
 
     data = await request.json()
+
     latest_ppg = data.get("ppg", [])
 
-    print("Received:", len(latest_ppg))
+    print("Received samples:", len(latest_ppg))
 
     return {"status": "ACK"}
-
 
 @app.get("/data")
 def get_data():
