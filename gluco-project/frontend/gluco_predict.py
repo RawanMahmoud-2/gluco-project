91628231dc24907e97b435d5e2168d107c61d7cd
RawanMahmoud-2 <rawan.mahmoud1231233@gmail.com>
2026-05-26 19:53:02 +0300

    Update gluco_predict.py

--- gluco-project/frontend/gluco_predict.py
+++ gluco-project/frontend/gluco_predict.py
@@ -13,6 +13,7 @@ def glucose_predict(ppg):
     checkpoint = torch.load(
         "cnn_gru_model_full_enhanced_600Epochs.pth",
         map_location=device
+         weights_only=False
     )
 
     # 3. Rebuild model and load weights
